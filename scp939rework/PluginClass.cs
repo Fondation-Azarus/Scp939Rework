25b3526aca85da45ed20daec557345ce9e2f8d62
Bonjemus <59416055+Bonjemus@users.noreply.github.com>
2022-05-14 12:46:16 +0200

    v1.0.0

---
+++ scp939rework/PluginClass.cs
@@ -0,0 +1,26 @@
+﻿using Synapse.Api.Plugin;
+
+namespace Scp939Rework
+{
+    [PluginInformation(
+        Name = "Scp939Rework",
+        Author = "Bonjemus",
+        Description = "A plug-in that adds features to SCP-939-XX.",
+        LoadPriority = 1,
+        SynapseMajor = SynapseController.SynapseMajor,
+        SynapseMinor = SynapseController.SynapseMinor,
+        SynapsePatch = SynapseController.SynapsePatch,
+        Version = "1.0.0"
+        )]
+    public class PluginClass : AbstractPlugin
+    {
+        [Config(section = "Scp939Rework")]
+        public static Configs Configs;
+
+        public override void Load()
+        {
+            if (!Configs.disabled)
+                new EventHandler();
+        }
+    }
+}
