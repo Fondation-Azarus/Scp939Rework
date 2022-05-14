4ea92c4306ae51c50d4bc0a923b259c7e9b69652
Bonjemus <59416055+Bonjemus@users.noreply.github.com>
2022-05-14 12:45:08 +0200

    v1.0.0

---
+++ scp939rework/Configs.cs
@@ -0,0 +1,76 @@
+﻿using Synapse.Config;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using Synapse.Api.Enum;
+
+namespace Scp939Rework
+{
+    public class Configs : AbstractConfigSection
+    {
+        [Description("Disabled ? :")]
+        public bool disabled = false;
+
+        [Description("The broadcast SCP-939-XXs receive when they spawn :")]
+        public string bcMessage = "The lower your health is, the more you inflict damages, you also get helpful effects when receiving damage.";
+
+        [Description("The duration of the broadcast (0 to disable) :")]
+        public ushort bcDuration = 7;
+
+        [Description("Start health (put a negative value if you don't want this plug-in to affect start health) :")]
+        public float health = -1;
+
+        [Description("Max health (put a negative value if you don't want this plug-in to affect max health) :")]
+        public float maxHealth = -1;
+
+
+        [Description("SCP-939-XXs' scale :")]
+        public Vector3 scale = Vector3.one;
+
+
+        [Description("What effects do SCP-939-XXs get when they spawn (duration: -1 = infinity) ? :")]
+        public List<EffectConfig> spawnEffectConfigs = new List<EffectConfig>()
+        {
+            new EffectConfig() { effect = Effect.MovementBoost, intensity = 20, duration = 10 },
+            new EffectConfig() { effect = Effect.DamageReduction, intensity = 30, duration = -1},
+        };
+
+        //[Description("The amount of health SCP-939-XX regenerates when killing (works with negative values) :")]
+        //public float healthKillRegen = 20; just use BetterScp :T
+
+
+        [Description("The base amount of damage SCP-939-XX's attacks deal (put a negative value if you don't want this plug-in to affect base damage) :")]
+        public float damageAmount = -1;
+
+        [Description("x value (put 0 or less to disable damage multiplier based on health) :")]
+        public float xValue = 2.5f;
+
+        [Description("The maximum distance between 2 SCP-939-XXs for them to receive extra damage :")]
+        public float maxDistance = 10;
+
+        [Description("The multiplier which affects damage if 2 SCP-939-XXs are close enough (put 1 to disable) :")]
+        public float damageMultiplier = 1.1f;
+
+        [Description("What effects do SCP-939-XXs inflict (duration: -1 = infinity) ? :")]
+        public List<EffectConfig> inflictEffectConfigs = new List<EffectConfig>()
+        {
+            new EffectConfig() { effect = Effect.Hemorrhage, intensity = 1, duration = 5 }
+        };
+
+        [Description("The amount of stamina the victim receives (can be negative) :")]
+        public float staminaBoost = 100;
+
+        [Description("What effects do SCP-939-XXs get when they receive damage (duration: -1 = infinity) ? :")]
+        public List<EffectConfig> damageEffectConfigs = new List<EffectConfig>()
+        {
+            new EffectConfig() { effect = Effect.MovementBoost, intensity = 30, duration = 5 }
+        };
+
+        public struct EffectConfig
+        {
+            public Effect effect;
+            public byte intensity;
+            public float duration;
+        }
+    }
+}
---
+++ scp939rework/EventHandler.cs
@@ -0,0 +1,79 @@
+﻿using Synapse;
+using Synapse.Api;
+using Synapse.Api.Events.SynapseEventArguments;
+using UnityEngine;
+using System.Linq;
+
+namespace Scp939Rework
+{
+    public class EventHandler
+    {
+        public EventHandler()
+        {
+            Server.Get.Events.Player.PlayerSetClassEvent += OnSetClass;
+            Server.Get.Events.Player.PlayerDamageEvent += OnDamage;
+        }
+
+        private bool Is939(int roleID) => roleID == (int)RoleType.Scp93953 || roleID == (int)RoleType.Scp93989;
+
+        private void OnSetClass(PlayerSetClassEventArgs ev)
+        {
+            try
+            {
+                MEC.Timing.CallDelayed(0.3f, () =>
+                {
+                    if (ev.Player == null || !Is939(ev.Player.RoleID))
+                        return;
+
+                    if (PluginClass.Configs.bcDuration > 0)
+                        ev.Player.SendBroadcast(PluginClass.Configs.bcDuration, PluginClass.Configs.bcMessage);
+
+                    if (PluginClass.Configs.maxHealth >= 0)
+                        ev.Player.Health = PluginClass.Configs.maxHealth;
+
+                    if (PluginClass.Configs.health >= 0)
+                        ev.Player.Health = PluginClass.Configs.health;
+
+                    ev.Player.Scale = PluginClass.Configs.scale;
+
+                    if (PluginClass.Configs.spawnEffectConfigs != null)
+                        foreach (Configs.EffectConfig ec in PluginClass.Configs.spawnEffectConfigs)
+                            ev.Player.GiveEffect(ec.effect, ec.intensity, ec.duration);
+                });
+            }
+            catch (System.Exception e)
+            {
+                Server.Get.Logger.Error(e);
+            }
+        }
+
+        private void OnDamage(PlayerDamageEventArgs ev)
+        {
+            if (ev.Victim == null) return;
+
+            if (ev.Killer != null && Is939(ev.Killer.RoleID)) //ev.DamageType == DamageType.Scp939) bugged
+            {
+                if (PluginClass.Configs.damageAmount >= 0)
+                    ev.Damage = PluginClass.Configs.damageAmount;
+
+                if (PluginClass.Configs.xValue > 0)
+                    ev.Damage *= PluginClass.Configs.xValue - ev.Killer.Health / ev.Killer.MaxHealth * (PluginClass.Configs.xValue - 1);
+
+                Player second939 = Server.Get.Players.FirstOrDefault(p => p != ev.Killer && Is939(p.RoleID));
+                if (PluginClass.Configs.damageMultiplier != 1 && second939 != null)
+                    if (Vector3.Distance(ev.Killer.Position, second939.Position) <= PluginClass.Configs.maxDistance)
+                        ev.Damage *= PluginClass.Configs.damageMultiplier;
+
+                foreach (Configs.EffectConfig ec in PluginClass.Configs.inflictEffectConfigs)
+                    ev.Victim.GiveEffect(ec.effect, ec.intensity, ec.duration);
+            }
+
+            else if (Is939(ev.Victim.RoleID))
+            {
+                ev.Victim.Stamina += PluginClass.Configs.staminaBoost;
+                foreach (Configs.EffectConfig ec in PluginClass.Configs.damageEffectConfigs)
+                    ev.Victim.GiveEffect(ec.effect, ec.intensity, ec.duration);
+            }
+        }
+    }
+}
